0a643fb5bdcb0bbbb359da574cf3fd794829be9b
Tejasvini Tummuru <tt26586@cs.utexas.edu>
2021-10-05 15:35:38 -0500

    trial run

--- check_heap.c
+++ check_heap.c
@@ -3,6 +3,8 @@
 
 //Place any variables needed here from umalloc.c as an extern.
 extern memory_block_t *free_head;
+int totalf = 0;
+extern int ftotal;
 
 /*
  * check_heap -  used to check that the heap is still in a consistent state.
@@ -13,14 +15,62 @@ extern memory_block_t *free_head;
 int check_heap() {
     // Example heap check:
     // Check that all blocks in the free list are marked free.
-    // If a block is marked allocated, return -1.
-    /*
+    // If a block is marked allocated, return -1. (Q1)
         memory_block_t *cur = free_head;
-        while (cur) {
+        //goes until it reached the end of the list
+        while (cur->next != NULL) {
+            //if a block is allocated or not free on the bfree list, immedietly no
             if (is_allocated(cur)) {
                 return -1;
             }
+            //progresses the pointer
+            cur = cur->next;
         }
-    */
+    //Checking that all the boxes that each item in the list points to is also a free block
+    //if a block is pointing to a block that is not free, -1 is returned (Q5)
+        //goes until the next has no next to check
+        while (cur->next != NULL){
+            //if the nect is allocated, immedietly no
+            if(is_allocated(cur->next)){
+                return -1;
+            }
+            //else keeps progressing the pointer
+            cur = cur->next;
+        }
+
+    //checking to see if every free block is on the free list
+        //goes through every free block on the free list and adds the total space that is on the list (Q2)
+        while (cur->next != NULL){
+            totalf+=get_size(cur);
+            cur = cur->next;
+        }
+        //compares the global variable that keeps track of the total amount of free space and sees if there is
+        //the same amount of free space on the list and if there is and if not, returns -1
+
+        if(ftotal != totalf){
+            return -1;
+        } 
+    //checking to see if the free list is in memory order (should i add and see if its still the same)
+        //goes through every single block on the free list until it reaches the end (Q10)
+        while (cur->next != NULL){
+            //if the size of the previous is bigger than the next, then the list is not being ordered in 
+            //ascending order, so it it not inserting them in the right order and immedietly returns -1
+            if(get_size(cur) > get_size(cur->next)){
+                return -1;
+            }
+        }  
+    //checking to see if the blocks are aligned correctly (Q3)
+        //goes through every single block on the free list until it reaches the end (Q10)
+        while (cur->next != NULL) {
+            //if a block is aligned to the 16 bit size it returns -1 if not
+            if(get_size(cur) % 16 != 0){
+                return-1;
+            }
+            //progresses the pointer
+            cur = cur->next;
+        }
+        
+    
+        
     return 0;
 }
--- umalloc.c
+++ umalloc.c
@@ -13,6 +13,7 @@ const char author[] = ANSI_BOLD ANSI_COLOR_RED "TEJASVINI TUMMURU TT26586" ANSI_
 
 // A sample pointer to the start of the free list.
 memory_block_t *free_head;
+int ftotal = 0;
 
 /*
  * is_allocated - returns true if a block is marked as allocated.
@@ -29,7 +30,6 @@ void allocate(memory_block_t *block) {
     assert(block != NULL);
     block->block_size_alloc |= 0x1;
 }
-
 
 /*
  * deallocate - marks a block as unallocated.
@@ -97,7 +97,6 @@ memory_block_t *find(size_t size) {
     memory_block_t *cur = free_head;
     bool found = false;
     while(cur->next != NULL && found == false){
-        printf("entering the loop");
         if(get_size(cur) >= size){
             found = true;
             return cur;
@@ -108,10 +107,30 @@ memory_block_t *find(size_t size) {
 }
 
 /*
+*finds the previous of a current
+*/
+memory_block_t *findBefore(memory_block_t *block){
+    memory_block_t *cur = block;
+    memory_block_t *prev = free_head;
+    while(prev->next != NULL && prev != cur){
+        if(prev->next == cur){
+            return prev;
+        }
+        prev = prev->next;
+    }
+    if(prev == cur){
+        return cur;
+    }
+    return NULL;
+}
+
+
+/*
  * extend - extends the heap if more memory is required.
  * todo
  */
 memory_block_t *extend(size_t size) {
+    ftotal+=16*PAGESIZE;
     return csbrk(16 * PAGESIZE);
 }
 
@@ -120,8 +139,22 @@ memory_block_t *extend(size_t size) {
  * todo
  */
 memory_block_t *split(memory_block_t *block, size_t size) {
-
-    return NULL;
+    //putting a block of this size into the list
+    //return free
+    //create a temporary pointer 
+    memory_block_t *temp = block; 
+    memory_block_t *bfree = block;
+    //get the pointer to a place on the block where we want to split using pointer arithmatic
+    //the components of this are the original temp + the header size(because I believe  its not included) + 
+    //(the size of the block - the size we want allocated since we do not want to reassign the pointers),, 
+    //if there is an error check if we add temp to itself,, do i call malloc on this? you don't because i would call split 
+    //in malloc which would lead to a lgic error
+    temp = (memory_block_t*) ((char*)temp + (sizeof(memory_block_t) + (get_size(block) - size)));
+    //once we have moved the pointer, we want to clarify this as a new block that is taken
+    put_block(temp, get_size(temp), true);
+    //make sure the other block is free, but do we need to put and return another block for that?
+    put_block(bfree, get_size(block) - sizeof(temp), false);
+    return temp;
 }
 
 /*
@@ -129,7 +162,31 @@ memory_block_t *split(memory_block_t *block, size_t size) {
  * todo
  */
 memory_block_t *coalesce(memory_block_t *block) {
-    return NULL;
+    //make sure the payload is being added correctly and the temp is getting the right values
+    //make sure the next and prev are right next to each other by adding the size the header and size of the payload and seeing if it adds to the next
+    memory_block_t *bfree = block; 
+    memory_block_t *temp = (memory_block_t*)((char*)bfree + sizeof(memory_block_t) + get_size(block));
+    memory_block_t *sbnext = block->next;
+    if(temp == block && !is_allocated(bfree)){
+        bfree = findBefore(block);
+        bfree = (memory_block_t*)((char*) bfree + sizeof(memory_block_t) + get_size(block));
+        put_block(bfree, get_size(bfree) + get_size(block) + sizeof(memory_block_t), false);
+        bfree->next = sbnext;
+    }
+    bfree = block->next;
+    if(block->next != NULL){
+            sbnext = bfree->next;
+    }
+    //pointers can't move backwards can they? this isn't illegal tho?
+    temp = (memory_block_t*)((char*)bfree - (sizeof(memory_block_t) + get_size(block)));
+    if(temp == block && !is_allocated(block->next)){
+        bfree = (memory_block_t*)((char*) bfree + sizeof(memory_block_t) + get_size(block));
+        put_block(bfree, get_size(bfree) + get_size(block) + sizeof(memory_block_t), false);
+        if(block->next != NULL){
+            bfree->next = sbnext;
+        }
+    }
+    return bfree;
 }
 
 
@@ -141,6 +198,7 @@ memory_block_t *coalesce(memory_block_t *block) {
  */
 int uinit() {
     free_head = csbrk(PAGESIZE * 12);
+    ftotal += PAGESIZE * 12;
     put_block(free_head, PAGESIZE * 12, false);
     // free_head->next = NULL;
     //set size, next, 
@@ -157,13 +215,25 @@ int uinit() {
  * todo
  */
 void *umalloc(size_t size) {
-
+    
     memory_block_t *cur = find(size);
     if(cur == NULL){
         cur = extend(size);
     }
+    if (get_size(cur) > size){
+        cur = split(cur, size);
+    }
+    // else if(get_size(cur) < size){
+    //     cur = coalesce(cur);
+    // }
     put_block(cur, get_size(cur), true);
     allocate(cur);
+    //must remove from free liost
+    memory_block_t *prev = findBefore(cur);
+    if(cur->next != NULL){
+        prev->next = cur->next;
+    }
+    ftotal-=size;
     return get_payload(cur);
 
     // memory_block_t *cur = free_head;
@@ -172,7 +242,6 @@ void *umalloc(size_t size) {
     // bool found = false;
 
     // while (found == false){
-    //     printf("entering the loop");
     //     if(get_size(cur)- sizeof(memory_block_t) == size){ //or do we do get_size() here?
     //         found = true;
     //         allocate(cur); //take it out of free list
@@ -197,7 +266,7 @@ void *umalloc(size_t size) {
     //         cur = cur->next; //loop
     //         //and then return a new block  
     //     }
-    // }
+    //}
     
     
     return NULL;
@@ -209,25 +278,22 @@ void *umalloc(size_t size) {
  * todo
  */
 void ufree(void *ptr) {
-    // memory_block_t *compare = get_block(ptr);
-    // deallocate(ptr);
-    // put_block(compare, get_size(compare), is_allocated(compare));
-    // //get_block to translate to payload->block
-    // //put_block to make free block and then while loop
-    // memory_block_t *cur = free_head;
-    // //do i just call put instead of a;ll this?
-    // bool added = false;
-    // while (cur->next != NULL && added == false){
-    //     printf("going in and not coming out");
-    //     //just compare pointers
-    //     if(compare < cur){
-    //         compare->next = cur->next;
-    //         cur->next = ptr;
-    //         added = true;
-    //         return;
-    //     }
-    //     cur = cur->next;
-    // }
-    // cur->next = ptr;
-    //where do we access the list/add to the free list?
+    memory_block_t *compare = get_block(ptr);
+    deallocate(compare);
+    put_block(compare, get_size(compare), is_allocated(compare));
+    //get_block to translate to payload->block
+    //put_block to make free block and then while loop
+    memory_block_t *cur = free_head;
+    //do i just call put instead of a;ll this?
+    bool added = false;
+    while (cur->next != NULL && added == false){
+        //just compare pointers
+        if(compare < cur){
+            //change prev?
+            compare->next = cur;
+            added = true;
+            return;
+        }
+        cur = cur->next;
+    }
 }
